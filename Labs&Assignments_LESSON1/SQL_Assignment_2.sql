399409c91c90ef8aa35a5ff20aa11d555d91cda3
PHThuan24 <phamhongthuan2401@gmail.com>
2023-11-30 12:42:02 +0700

    Submit Labs-Assignments Lesson 1

---
+++ Labs&Assignments_LESSON1/SQL_Assignment_2.sql
@@ -0,0 +1,83 @@
+CREATE DATABASE ManagementFresher;
+
+-- DROP DATABASE ManagementFresher;
+
+USE ManagementFresher;
+
+-- Question 1: Tạo table với các ràng buộc và kiểu dữ liệu
+CREATE TABLE Trainee(
+	trainee_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    full_name NVARCHAR(150) NOT NULL,
+    birth_day DATE,
+    gender ENUM('male', 'female', 'unknown'),
+    et_iq INT DEFAULT 0,
+    et_gmath INT DEFAULT 0,
+    et_english INT DEFAULT 0,
+    training_class NVARCHAR(50),
+    evaluation_notes NVARCHAR(250)
+);
+
+INSERT INTO Trainee (full_name, birth_day, gender, et_iq, 
+et_gmath, et_english, training_class, evaluation_notes)
+VALUES ('Pham Hong Thuan 1', '2000-01-24', 'male', 8, 9, 8, 'IT001', 'No');
+
+INSERT INTO Trainee (full_name, birth_day, gender, 
+			training_class, evaluation_notes)
+VALUES ('Pham Hong Thuan 2', '2000-01-24', 'male',  'IT001', 'No');
+
+SELECT * FROM Trainee;
+
+-- Question 2: thêm trường VTI_Account với điều kiện not null & unique
+ALTER TABLE Trainee
+ADD COLUMN vti_account NVARCHAR(100) NOT NULL;
+
+-- Chưa add được UNIQUE || Error Code: 1062. Duplicate entry '' for key 'trainee.UC_vti_account'
+ALTER TABLE Trainee
+ADD CONSTRAINT UC_vti_account UNIQUE(vti_account);
+
+
+INSERT INTO Trainee (vti_account, full_name, birth_day, gender, 
+			training_class, evaluation_notes)
+VALUES ('thuanph03','Pham Hong Thuan 3', '2000-01-24', 'male',  'IT001', 'No');
+
+-- Exercise 2: Data Types
+CREATE TABLE Exercise2(
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    namess VARCHAR(100),
+    codes CHAR(5),
+    modified_date DATETIME
+);
+
+INSERT INTO Exercise2(namess, codes, modified_date)
+VALUES ('John', 'CODE1', '2023-11-30 9:55:12');
+
+INSERT INTO Exercise2(namess, codes)
+VALUES ('Jan', 'CODE2');
+
+SELECT * FROM Exercise2;
+
+-- Exercise 3: Data Types (2)
+CREATE TABLE Exercise3(
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    namess VARCHAR(100),
+    birth_day DATE,
+    gender BIT,
+    is_delete_flag ENUM('0', '1')
+);
+
+INSERT INTO Exercise3(namess, birth_day, gender, is_delete_flag)
+VALUES ('John', '2003-11-30', 0, '1');
+INSERT INTO Exercise3(namess, birth_day, gender, is_delete_flag)
+VALUES ('Jan', '2003-01-10', 1, '0');
+
+SELECT  namess, birth_day, 
+CASE 
+	WHEN gender = 0 THEN 'male'
+    WHEN gender = 1 THEN 'female'
+    ELSE 'unknown'
+END AS gender,
+CASE 
+	WHEN is_delete_flag = '0' THEN 'đang hoạt động,'
+    ELSE 'đã xóa'
+END AS is_delete_flag
+FROM Exercise3;
---
+++ Labs&Assignments_LESSON1/Testing_System_Assignment_1.sql
@@ -0,0 +1,95 @@
+CREATE DATABASE ManagementExam;
+
+USE ManagementExam;
+
+CREATE TABLE Department(
+	department_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name NVARCHAR(150) NOT NULL
+);
+
+CREATE TABLE Positions(
+	position_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    position_name ENUM('Dev', 'Test', 'Scrum', 'Master', 'PM')
+);
+
+CREATE TABLE Accounts(
+	account_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100),
+    user_name NVARCHAR(100) NOT NULL,
+    full_name NVARCHAR(100),
+    department_id BIGINT NOT NULL,
+	position_id INT NOT NULL,
+    create_date DATE,
+	FOREIGN KEY (department_id) REFERENCES Department(department_id),
+    FOREIGN KEY (position_id) REFERENCES Positions(position_id)
+);
+
+
+CREATE TABLE Groupss(
+	group_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    group_name NVARCHAR(150) NOT NULL,
+    creator_id BIGINT NOT NULL,
+    create_date DATE,
+    FOREIGN KEY (creator_id) REFERENCES Accounts(account_id)
+);
+
+CREATE TABLE GroupAccount(
+    group_id BIGINT NOT NULL,
+    account_id BIGINT NOT NULL,
+	join_date DATE,
+	FOREIGN KEY (group_id) REFERENCES Groupss(group_id),
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
+);
+
+-- ALTER TABLE GroupAccount
+-- ADD COLUMN group_account_id BIGINT AUTO_INCREMENT,
+-- ADD CONSTRAINT group_account_id  PRIMARY KEY (group_id,account_id);
+
+CREATE TABLE TypeQuestion(
+	type_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    type_name ENUM('Essay', 'Multiple-Choice')
+);
+
+CREATE TABLE CategoryQuestion(
+	category_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_name NVARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Question(
+	question_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    content NVARCHAR(250) NOT NULL,
+    category_id BIGINT NOT NULL,
+    type_id BIGINT NOT NULL,
+    creator_id BIGINT NOT NULL,
+    create_date DATE,
+    FOREIGN KEY (category_id) REFERENCES CategoryQuestion(category_id),
+    FOREIGN KEY (type_id) REFERENCES TypeQuestion(type_id),
+    FOREIGN KEY (creator_id) REFERENCES Accounts(account_id)
+);
+
+CREATE TABLE Answer(
+	answer_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    content NVARCHAR(500) NOT NULL,
+    question_id BIGINT NOT NULL,
+    is_correct BOOLEAN,
+	FOREIGN KEY (question_id) REFERENCES Question(question_id)
+);
+
+CREATE TABLE Exam(
+	exam_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    codes VARCHAR(50),
+    title NVARCHAR(150),
+    category_id BIGINT NOT NULL,
+    duration TIME,
+    creator_id BIGINT NOT NULL,
+    create_date Date,
+    FOREIGN KEY (category_id) REFERENCES CategoryQuestion(category_id),
+    FOREIGN KEY (creator_id) REFERENCES Accounts(account_id)
+);
+
+CREATE TABLE ExamQuestion(
+	exam_id BIGINT NOT NULL,
+    question_id BIGINT NOT NULL,
+    FOREIGN KEY (exam_id) REFERENCES Exam(exam_id),
+    FOREIGN KEY (question_id) REFERENCES Question(question_id)
+);
